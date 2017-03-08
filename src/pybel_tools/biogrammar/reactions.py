4a622cd33611893f9599ede7ac7ae9596294db4f
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-03-08 01:23:45 +0100

    Added Brenda reaction data source link

--- src/pybel_tools/biogrammar/reactions.py
+++ src/pybel_tools/biogrammar/reactions.py
@@ -6,3 +6,9 @@ This module checks that the substrates and products of reactions are feasible/co
 2. Download reaction data from (source??)
 
 """
+
+#: ID, EC Number, Reccomended Name, Reaction, Rxn ID Brenda, Rxn ID KEGG,
+#: Rxn ID MetaCyc, Rxn ID SABIO-RK, Brenda Pathway Name, KEGG Pathway ID
+#: Kegg pathway name, MetaCyc pathway ID, MetaCyc Pathway Name, Stoichiometry Check
+#: Missing Substrate, Missing Produt, Commentary KEGG, Commentary MetaCyc, Remark
+BKMS_REACT_DATA = 'http://bkm-react.tu-bs.de/download/Reactions_BKMS.tar.gz'
