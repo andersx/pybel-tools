ec0884408e9988699f2b959fcd7610130491e359
lekono <andrej.konotopez@scai.fraunhofer.de>
2017-03-29 15:12:48 +0200

    Route definitions for db_service

--- src/pybel_tools/service/db_service.py
+++ src/pybel_tools/service/db_service.py
@@ -28,20 +28,109 @@ def build_database_service_app(dsa):
     api = DatabaseService()
     dsa.config[DATABASE_SERVICE] = api
 
-    @dsa.route('/networks')
+    @dsa.route('/networks', methods=['GET'])
     def list_networks():
         pass
 
-    @dsa.route('/namespaces')
-    def func():
+    @dsa.route('/namespaces', methods=['GET'])
+    def list_namespaces():
         pass
 
     @dsa.route('/namespaces/by_network/<int:network_id>', methods=['GET'])
-    def func():
+    def namespaces_by_network(network_id):
         pass
 
-    @dsa.route('/names/<str:definition_key>', methods=['GET'])
-    def func():
+    @dsa.route('/namespace/<definition_key>', methods=['GET'])
+    def list_names(definition_key):
+        pass
+
+    @dsa.route('/annotations', methods=['GET'])
+    def list_annotations():
+        pass
+
+    @dsa.route('/annotations/by_network/<int:network_id>', methods=['GET'])
+    def annotations_by_network(network_id):
+        pass
+
+    @dsa.route('/annotation/<definition_key>', methods=['GET'])
+    def list_annotation_names():
+        pass
+
+    @dsa.route('/citations', methods=['GET'])
+    def list_citations():
+        pass
+
+    @dsa.route('/citations/by_network/<int:network_id>', methods=['GET'])
+    def citations_by_network(network_id):
+        pass
+
+    @dsa.route('/edges/by_bel/statement/<statement_bel>', methods=['GET'])
+    def edges_by_bel_statement(statement_bel):
+        pass
+
+    @dsa.route('/edges/by_bel/source/<source_bel>', methods=['GET'])
+    def edges_by_bel_source(source_bel):
+        pass
+
+    @dsa.route('/edges/by_bel/target/<target_bel>', methods=['GET'])
+    def edges_by_bel_target(target_bel):
+        pass
+
+    @dsa.route('/edges/by_network/<int:network_id>', methods=['GET'])
+    def edges_by_network(network_id):
+        pass
+
+    @dsa.route('/edges/by_pmid/<int:pmid_id>', methods=['GET'])
+    def edges_by_pmid(pmid_id):
+        pass
+
+    @dsa.route('/edges/by_author/<author>', methods=['GET'])
+    def edges_by_author(author):
+        pass
+
+    @dsa.route('/edges/by_network_and_author/<int:network_id>/<author>', methods=['GET'])
+    def edges_by_network_and_author(network_id, author):
+        pass
+
+    @dsa.route('/edges/by_network_and_pmid/<int:network_id>/<int:pmid_id>', methods=['GET'])
+    def edges_by_network_and_pmid(network_id, pmid_id):
+        pass
+
+    @dsa.route('/edges/by_annotation/<annotation_name>/<annotation_value>', methods=['GET'])
+    def edges_by_annotation(annotation_name, annotation_value):
+        pass
+
+    @dsa.route('/edges/by_network_and_annotation/<int:network_id>/<annotation_name>/<annotation_value>',
+               methods=['GET'])
+    def edges_by_network_and_annotation(network_id, annotation_name, annotation_value):
+        pass
+
+    @dsa.route('/nodes/by_bel/<node_bel>', methods=['GET'])
+    def nodes_by_bel(node_bel):
+        pass
+
+    @dsa.route('/nodes/by_name/<node_name>', methods=['GET'])
+    def nodes_by_name(node_name):
+        pass
+
+    @dsa.route('/nodes/by_namespace/<node_namespace>', methods=['GET'])
+    def nodes_by_namespace(node_namespace):
+        pass
+
+    @dsa.route('/nodes/by_network/<int:network_id>', methods=['GET'])
+    def nodes_by_network(network_id):
+        pass
+
+    @dsa.route('/path/by_bel/start_node/<node_bel>', methods=['GET'])
+    def paths_by_start(node_bel):
+        pass
+
+    @dsa.route('/path/by_bel/end_node/<node_bel>', methods=['GET'])
+    def paths_by_end(node_bel):
+        pass
+
+    @dsa.route('/path/by_bel/start_and_end/<start_bel>/<end_bel>', methods=['GET'])
+    def paths(start_bel, end_bel):
         pass
 
 
