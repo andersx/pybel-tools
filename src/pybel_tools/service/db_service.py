357dd96178b4c6789b48f257d8d58ab97c04f87d
lekono <andrej.konotopez@scai.fraunhofer.de>
2017-03-29 14:49:11 +0200

    Added db_service basics

--- src/pybel_tools/service/db_service.py
+++ src/pybel_tools/service/db_service.py
@@ -1,11 +1,49 @@
 # -*- coding: utf-8 -*-
 
 """
-This module contains ll of the services necessary through the PyBEL API Definition, backed by the PyBEL cache manager.
+This module contains all of the services necessary through the PyBEL API Definition, backed by the PyBEL cache manager.
 """
 
-from .base_service import PybelService
+# from .base_service import PybelService
+from flask import Flask
+
+from .db_service_utils import DatabaseService
+
+# class DatabaseService(PybelService):#
+
+#    def __init__(self):
+#        pass
+
+DATABASE_SERVICE = 'database_service'
 
 
-class DatabaseService(PybelService):
-    pass
+def build_database_service_app(dsa):
+    """Builds the PyBEL Dictionary-Backed API Service. Adds a latent PyBEL Dictionary service that can be retrieved
+    with :func:`get_dict_service`
+
+    :param dsa: A Flask App
+    :type dsa: Flask
+    """
+
+    api = DatabaseService()
+    dsa.config[DATABASE_SERVICE] = api
+
+    @dsa.route('/networks')
+    def list_networks():
+        pass
+
+    @dsa.route('/namespaces')
+    def func():
+        pass
+
+    @dsa.route('/namespaces/by_network/<int:network_id>', methods=['GET'])
+    def func():
+        pass
+
+    @dsa.route('/names/<str:definition_key>', methods=['GET'])
+    def func():
+        pass
+
+
+app = Flask(__name__)
+build_database_service_app(app)
---
+++ src/pybel_tools/service/db_service_utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module contains all of the services necessary through the PyBEL API Definition, backed by the PyBEL cache manager.
+"""
+
+from .base_service import PybelService
+
+
+class DatabaseService(PybelService):
+    def __init__(self):
+        pass
