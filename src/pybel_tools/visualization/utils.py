74873e4fe5c22e9c36fdb802e11ec4669a5f3b92
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-04-03 23:24:04 +0200

    Added python-backed color map switching
    Closes #30
    References #35

--- src/pybel_tools/visualization/utils.py
+++ src/pybel_tools/visualization/utils.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 import jinja2
+
+from pybel.constants import *
+
+__all__ = [
+    'render_template',
+    'default_color_map'
+]
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -19,3 +24,18 @@ def render_template(template_filename, context=None):
     if context is None:
         context = {}
     return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+
+
+#: The color map defining the node colors in visualization
+default_color_map = {
+    PROTEIN: "#1F77B4",
+    PATHOLOGY: "#FF7F0E",
+    BIOPROCESS: "#2CA02C",
+    MIRNA: "#D62728",
+    COMPLEX: "#9467bd",
+    COMPOSITE: "#9467bd",
+    REACTION: "#8c564b",
+    GENE: "#e377c2",
+    ABUNDANCE: "#bcbd22",
+    RNA: "#17becf"
+}
