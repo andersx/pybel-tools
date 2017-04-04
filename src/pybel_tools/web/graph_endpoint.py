929d949ddab958302b95a64e9bee3a4a92184148
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-04-04 14:36:43 +0200

    Update graph endpoints

--- src/pybel_tools/web/graph_endpoint.py
+++ src/pybel_tools/web/graph_endpoint.py
@@ -16,13 +16,11 @@ log = logging.getLogger(__name__)
 
 
 # TODO add getter function for given network in variety of formats @ddomingof @lekono @cthoyt
-def build_graph_endpoint(app, prefix='/networks/'):
+def build_graph_endpoint(app):
     """Adds common access to the graph cache
     
     :param app: A Flask app
     :type app: flask.Flask
-    :param prefix:
-    :type: str
     """
     gcm = get_graph_manager(app)
 
@@ -31,9 +29,20 @@ def build_graph_endpoint(app, prefix='/networks/'):
         """Returns JSON of the network id's to their name and version"""
         return jsonify({gid: {METADATA_NAME: name, METADATA_VERSION: version} for gid, name, version in gcm.ls()})
 
+    log.info('Added PyBEL Web graph endpoint to %s', app)
+
+
+def build_graph_viewer(app, prefix='/networks'):
+    """Adds common access to the graph cache
+
+    :param app: A Flask app
+    :type app: flask.Flask
+    :param prefix: The 
+    :type prefix: str
+    """
+    gcm = get_graph_manager(app)
+
     @app.route('/networks/list')
     def list_networks_user():
         """Displays an HTML page for listing the networks"""
         return render_template('network_list.html', data=list(gcm.ls()), prefix=prefix)
-
-    log.info('Added PyBEL Web graph endpoint to %s', app)
