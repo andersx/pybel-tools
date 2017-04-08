795555f5a386c4575ac18883b8e8bea8a9dbad80
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-04-08 23:37:09 +0200

    Implement shortest path algorithms
    Closes #34

--- src/pybel_tools/selection/paths.py
+++ src/pybel_tools/selection/paths.py
@@ -53,6 +53,33 @@ def get_nodes_in_dijkstra_paths(graph, nodes, cutoff=None, weight='weight'):
     return set(chain.from_iterable(single_source_dijkstra_path(graph, n, cutoff=cutoff, weight=weight) for n in nodes))
 
 
+# TODO consider all shortest paths?
+def _get_shortest__path_between_subgraphs_helper(graph, a, b):
+    """Calculate the shortest path that occurs between two disconnected subgraphs A and B going through nodes in
+    the source graph
+
+    :param graph: A graph
+    :type graph: nx.MultiGraph
+    :param a: A subgraph of :code:`graph`, disjoint from :code:`b`
+    :type a: nx.MultiGraph
+    :param b: A subgraph of :code:`graph`, disjoint from :code:`a`
+    :type b: nx.MultiGraph
+    :return: A list of the shortest paths between the two subgraphs
+    :rtype: list
+    """
+    shortest_paths = []
+
+    for na, nb in itt.product(a.nodes_iter(), b.nodes_iter()):
+        a_b_shortest_path = nx.shortest_path(graph, na, nb)
+        shortest_paths.append(a_b_shortest_path)
+
+        b_a_shortest_path = nx.shortest_path(graph, nb, na)
+        shortest_paths.append(b_a_shortest_path)
+
+    min_len = min(map(len, shortest_paths))
+    return [p for p in shortest_paths if len(p) == min_len]
+
+
 def get_shortest_directed_path_between_subgraphs(graph, a, b):
     """Calculate the shortest path that occurs between two disconnected subgraphs A and B going through nodes in
     the source graph
@@ -63,20 +90,14 @@ def get_shortest_directed_path_between_subgraphs(graph, a, b):
     :type a: pybel.BELGraph
     :param b: A subgraph of :code:`graph`, disjoint from :code:`a`
     :type b: pybel.BELGraph
-    :return:
+    :return: A list of the shortest paths between the two subgraphs
+    :rtype: list
     """
-    shortest_paths = []
-    for na, nb in itt.product(a.nodes_iter(), b.nodes_iter()):
-        a_b_shortest_path = nx.shortest_path(graph, na, nb)
-        shortest_paths.append(a_b_shortest_path)
-        b_a_shortest_path = nx.shortest_path(graph, nb, na)
-        shortest_paths.append(b_a_shortest_path)
-    return min(shortest_paths, key=len)
+    return _get_shortest__path_between_subgraphs_helper(graph, a, b)
 
 
-# TODO implement
-def get_shortest_undirected_path_between_subgraphs(G, a, b):
-    """Get the shortest path between two disconnected subgraphs, disregarding directionality of edges in G
+def get_shortest_undirected_path_between_subgraphs(graph, a, b):
+    """Get the shortest path between two disconnected subgraphs A and B, disregarding directionality of edges in graph
 
     :param graph: A BEL graph
     :type graph: pybel.BELGraph
@@ -84,6 +105,8 @@ def get_shortest_undirected_path_between_subgraphs(G, a, b):
     :type a: pybel.BELGraph
     :param b: A subgraph of :code:`graph`, disjoint from :code:`a`
     :type b: pybel.BELGraph
-    :return:
+    :return: A list of the shortest paths between the two subgraphs
+    :rtype: list
     """
-    raise NotImplementedError
+    ug = graph.to_undirected()
+    return _get_shortest__path_between_subgraphs_helper(ug, a, b)
