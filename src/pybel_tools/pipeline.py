3de52d8d89fef6bce0309c2ba0197f510e60cb3e
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-04-08 11:37:56 +0200

    Update pipeline code

--- src/pybel_tools/pipeline.py
+++ src/pybel_tools/pipeline.py
@@ -2,75 +2,21 @@
 
 """This module assists in running complex workflows on BEL graphs"""
 
-from pybel import BELGraph
-from .summary.export import info_list
 from functools import wraps
-OPERATION_PROVENANCE = 'operation_provenance'
+
+__all__ = [
+    'PipelineBuilder',
+    'in_place_mutator',
+    'uni_in_place_mutator',
+    'uni_mutator',
+    'mutator'
+]
 
 
-# TODO make more strict for non-in place operations so it's possible to perform a functional "reduce"
-def transform(graph, *transformations):
-    """Applies a series of transformations to a graph
-
-    This function mixes the ability to perform in-place and copying operations, so it's
-    a good idea to assume the original graph isn't stable after applying this function.
-
-    :param graph: A BEL graph
-    :type graph: pybel.BELGraph
-    :return: A transformed BEL graph
-    :rtype: pybel.BELGraph
-    """
-
-    current_graph = graph
-
-    for transformation in transformations:
-        result = transformation(current_graph)
-        if isinstance(result, BELGraph):
-            current_graph = result
-
-    return current_graph
-
-
-def add_operation(graph, operation_info):
-    """Adds annotations to the graph about what transformations were applied
-
-    :param graph: A BEL graph
-    :type graph: pybel.BELGraph
-    :param operation_info: A dictionary containing information about the function that was applied
-    :type operation_info: dict
-    """
-    if OPERATION_PROVENANCE not in graph.graph:
-        graph.graph[OPERATION_PROVENANCE] = []
-    graph.graph[OPERATION_PROVENANCE].append(operation_info)
-
-
-def summarize_operation_list(graph, graph_function):
-    """Applies an operation to the graph then returns a list with the information about the change in the
-    graph's properties.
-
-    :param graph: A BEL graph
-    :type graph: pybel.BELGraph
-    :param graph_function: A mutation function that takes (graph, ) -> None
-    :type graph_function: lambda
-    :return: A list of the change in each aspect of the graph
-    :rtype: list
-    """
-    pre_list = info_list(graph)
-    graph_function(graph)
-    post_list = info_list(graph)
-
-    result = [pre_list[0]]
-
-    for (k, before), (_, after) in zip(pre_list, post_list):
-        result.append((k, after - before))
-
-    return result
-
-
-class Pipeline:
+class PipelineBuilder:
     """Builds and runs analytical pipelines on BEL graphs"""
 
-    def __init__(self, universe):
+    def __init__(self, universe=None):
         """
         :param universe: The entire set of known knowledge to draw from
         :type universe: pybel.BELGraph 
@@ -78,42 +24,61 @@ class Pipeline:
         self.universe = universe
         self.protocol = []
 
-    def wrap_universe_protocol(self, func):
+    def wrap_universe_protocol(self, f):
         """Takes a function that needs a universe graph as the first argument and returns a wrapped one"""
+        if self.universe is None:
+            raise ValueError('No universe is set in PipelineBuilder')
 
-        def universe_protocol(graph, argument):
-            """Applys the function with the contained universe graph"""
-            return func(self.universe, graph, argument)
+        @wraps(f)
+        def wrapper(graph, argument):
+            return f(self.universe, graph, argument)
 
-        universe_protocol.__doc__ = func.__doc__
+        return wrapper
 
-        return universe_protocol
+    @staticmethod
+    def wrap_in_place(f):
+        """Takes a function that doesn't return the graph and returns the graph"""
 
-    def wrap_in_place(self, func):
-        def wrapped(graph, *attrs, **kwargs):
-            func(graph, *attrs, **kwargs)
+        @wraps(f)
+        def wrapper(graph, *attrs, **kwargs):
+            f(graph, *attrs, **kwargs)
             return graph
 
-        return wrapped
+        return wrapper
 
-    def add_universe_protocol(self, func, *attrs, **kwargs):
+    def add_universe_protocol(self, f, *attrs, **kwargs):
         """Adds a function that takes the universe as its first argument to the protocol"""
-        self._add_protocol(True, False, self.wrap_universe_protocol(func), *attrs, **kwargs)
+        self._add_protocol(True, False, self.wrap_universe_protocol(f), *attrs, **kwargs)
 
-    def add_mutation_protocol(self, func, *attrs, **kwargs):
+    def add_mutation_protocol(self, f, *attrs, **kwargs):
         """Adds a function that mutates a graph to the protocol"""
-        self._add_protocol(False, False, func, *attrs, **kwargs)
+        self._add_protocol(False, False, f, *attrs, **kwargs)
 
-    def add_universe_protocol_inplace(self, func, *attrs, **kwargs):
+    def add_universe_protocol_inplace(self, f, *attrs, **kwargs):
         """Adds a function that takes the universe as its first argument to the protocol"""
-        self._add_protocol(True, True, self.wrap_in_place(self.wrap_universe_protocol(func)), *attrs, **kwargs)
+        self._add_protocol(True, True, self.wrap_in_place(self.wrap_universe_protocol(f)), *attrs, **kwargs)
 
-    def add_mutation_protocol_inplace(self, func, *attrs, **kwargs):
+    def add_mutation_protocol_inplace(self, f, *attrs, **kwargs):
         """Adds a function that mutates a graph to the protocol"""
-        self._add_protocol(False, True, self.wrap_in_place(func), *attrs, **kwargs)
+        self._add_protocol(False, True, self.wrap_in_place(f), *attrs, **kwargs)
 
-    def _add_protocol(self, wrapped, func, *attrs, **kwargs):
-        self.protocol.append((wrapped, func, attrs, kwargs))
+    def _add_protocol(self, universe, wrapped, f, *attrs, **kwargs):
+        self.protocol.append((universe, wrapped, f, attrs, kwargs))
+
+    def add_protocol(self, f, *attrs, **kwargs):
+        if not hasattr(f, 'wrap_universe'):
+            raise ValueError('{} is not wrapped with a pipeline decorator'.format(f.__name__))
+
+        if f.wrap_universe:
+            if f.wrap_in_place:
+                self.add_universe_protocol_inplace(f, *attrs, **kwargs)
+            else:
+                self.add_universe_protocol(f, *attrs, **kwargs)
+        else:
+            if f.wrap_in_place:
+                self.add_mutation_protocol_inplace(f, *attrs, **kwargs)
+            else:
+                self.add_mutation_protocol(f, *attrs, **kwargs)
 
     def run_protocol(self, graph):
         """Runs the contained protocol on a seed graph
@@ -122,21 +87,25 @@ class Pipeline:
         :type graph: pybel.BELGraph 
         """
         result = graph
-        for _, _, func, attrs, kwargs in self.protocol:
-            result = func(result, *attrs, **kwargs)
+        for _, _, f, attrs, kwargs in self.protocol:
+            result = f(result, *attrs, **kwargs)
         return result
 
     def print_summary(self, file=None):
         """Prints as summary of the protocol"""
-        for wrapped, func, attrs, kwargs in self.protocol:
-            print(wrapped, func, attrs, kwargs, file=file)
+        for universe, wrapped, func, attrs, kwargs in self.protocol:
+            print(universe, wrapped, func, attrs, kwargs, file=file)
 
     @staticmethod
-    def runnable(wrap_universe, wrap_in_place):
-        def decorator(func):
-            @wraps(func)
+    def mutator_decorator_builder(wrap_universe, wrap_in_place):
+        """Builds a decorator function to tag mutator functions"""
+
+        def decorator(f):
+            """A decorator that tags mutator functions"""
+
+            @wraps(f)
             def wrapper(*attrs, **kwargs):
-                func(*attrs, **kwargs)
+                f(*attrs, **kwargs)
 
             wrapper.wrap_universe = wrap_universe
             wrapper.wrap_in_place = wrap_in_place
@@ -146,7 +115,7 @@ class Pipeline:
 
 
 #: A function decorator to inform the Pipeline how to handle a function
-in_place_mutator = Pipeline.runnable(wrap_universe=False, wrap_in_place=True)
-uni_in_place_mutator = Pipeline.runnable(wrap_universe=True, wrap_in_place=True)
-uni_mutator = Pipeline.runnable(wrap_universe=True, wrap_in_place=False)
-mutator = Pipeline.runnable(wrap_universe=False, wrap_in_place=False)
+in_place_mutator = PipelineBuilder.mutator_decorator_builder(wrap_universe=False, wrap_in_place=True)
+uni_in_place_mutator = PipelineBuilder.mutator_decorator_builder(wrap_universe=True, wrap_in_place=True)
+uni_mutator = PipelineBuilder.mutator_decorator_builder(wrap_universe=True, wrap_in_place=False)
+mutator = PipelineBuilder.mutator_decorator_builder(wrap_universe=False, wrap_in_place=False)
