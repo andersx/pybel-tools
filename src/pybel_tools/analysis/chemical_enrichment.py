53e5ee496a59eef12ac0a2ecf33e0a8b812171e2
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-04-21 13:20:04 +0200

    Add chemical similarity enrichment stub
    [skip ci]

---
+++ src/pybel_tools/analysis/chemical_enrichment.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+Chemical Similarity Enrichment
+
+1. Extract all chemicals from CHEBI from BEL network
+2. Convert to database identifiers using external RESTFul services (CHEBI for chebi things)
+3. Query ChEBML or other service for similar compounds
+4. Annotate association relationships to chemicals
+
+"""
