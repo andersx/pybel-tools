e407ab85ead8ab6816791110d7630edfb1b4b33a
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-04-27 14:20:08 +0200

    Add function to get unused namespaces

--- src/pybel_tools/summary/node_summary.py
+++ src/pybel_tools/summary/node_summary.py
@@ -121,3 +121,15 @@ def get_names_with_errors(graph, namespace):
     :rtype: set[str]
     """
     return get_names(graph, namespace) | get_incorrect_names(graph, namespace)
+
+
+def get_unused_namespaces(graph):
+    """Gets all of the unused namespaces in a graph
+    
+    :param pybel.BELGraph graph: A BEL graph 
+    :return: A set of namespaces that are included but not used
+    :rtype: set[str]
+    """
+    used_namespaces = get_namespaces(graph)
+    all_namespaces = set(graph.namespace_url) | set(graph.namespace_owl) | set(graph.namespace_pattern)
+    return all_namespaces - used_namespaces
