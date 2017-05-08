e67541a22dee796966cfe28371bb2c83adb06490
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-05-08 12:23:38 +0200

    Update login service
    - add Daniel to administrators
    - add future strict login option

--- src/pybel_tools/web/login_service.py
+++ src/pybel_tools/web/login_service.py
@@ -4,7 +4,7 @@ import os
 
 import flask
 import requests
-from flask import Response, redirect, url_for, request, session, jsonify
+from flask import Response, redirect, url_for, request, session, jsonify, flash
 from flask_github import GitHub
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 
@@ -13,6 +13,10 @@ from .constants import PYBEL_GITHUB_CLIENT_ID, PYBEL_GITHUB_CLIENT_SECRET
 
 def get_github_info(token):
     return requests.get('https://api.github.com/user', params={'access_token': token}).json()
+
+
+administrator_ids = {5069736}
+administrator_usernames = {'cthoyt', 'ddomingof'}
 
 
 class User(UserMixin):
@@ -28,16 +32,17 @@ class User(UserMixin):
 
     @property
     def admin(self):
-        return self.github_id == 5069736
+        return self.github_id in administrator_ids or self.username in administrator_usernames
 
 
-def build_login_service(app):
+def build_login_service(app, strict_login=False):
     """Adds the login service
     
     Before adding this service, both ``GITHUB_CLIENT_ID`` and ``GITHUB_CLIENT_SECRET`` need to be set in the app's
     configuration
     
     :param flask.Flask app: A Flask app
+    :param bool strict_login: Should users be forced to add a name to their GitHub accounts to use PyBEL Web?
     """
     app.config.update({
         'GITHUB_CLIENT_ID': os.environ[PYBEL_GITHUB_CLIENT_ID],
@@ -72,6 +77,11 @@ def build_login_service(app):
             return redirect(next_url)
 
         user = User(access_token)
+
+        if strict_login and not user.name:
+            flash('Please add a name to your GitHub account to use PyBEL Web')
+            return redirect(url_for('view_networks'))
+
         login_user(user)
 
         return redirect(next_url)
@@ -80,7 +90,7 @@ def build_login_service(app):
     @login_required
     def logout():
         logout_user()
-        flask.flash('Logged out')
+        flash('Logged out')
         return redirect(url_for('view_networks'))
 
     @app.errorhandler(401)
