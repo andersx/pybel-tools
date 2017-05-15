06a377bf57faa263b5aeab6530fe1615f4efaf2b
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-05-15 12:06:24 +0200

    Add resources file
    [skip ci]

---
+++ src/pybel_tools/resources.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""This file contains the curated resource dictionary"""
+
+import pybel
+
+HGNC_HUMAN_GENES = 'https://arty.scai.fraunhofer.de/artifactory/bel/namespace/hgnc-human-genes/hgnc-human-genes-20170511.belns'
+CHEBI = 'https://arty.scai.fraunhofer.de/artifactory/bel/namespace/chebi/chebi-20170511.belns'
+CHEBI_IDS = 'https://arty.scai.fraunhofer.de/artifactory/bel/namespace/chebi-ids/chebi-ids-20170511.belns'
+
+CONFIDENCE = pybel.constants.FRAUNHOFER_RESOURCES + 'confidence.belanno'
