f2ec04c63e041a48224bf6ef417ea74a719159a9
ddomingofernandez <ddomingof@yahoo.es>
2017-05-16 09:17:44 +0200

    test added for list/complexes/composites

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -19,16 +19,26 @@ class TestMapping(unittest.TestCase):
         self.assertIn('APP', mapped_nodes)
         self.assertEqual({'APP Fragment'}, mapped_nodes['APP'])
 
-
     def test_complexes_composites_mapping(self):
         g = BELGraph()
 
-        g.add_node('APP', attr_dict={NAMESPACE: 'HGNC', NAME: 'APP'})
-        g.add_node('APP Fragment')
-        g.add_edge('APP', 'APP Fragment', **{RELATION: HAS_COMPONENT})
+        g.add_node('complex(p(HGNC:CCL2), p(HGNC:CCR2))')
+        g.add_node('CCL2', attr_dict={NAMESPACE: 'HGNC', NAME: 'CCL2'})
+        g.add_node('CCR2', attr_dict={NAMESPACE: 'HGNC', NAME: 'CCR2'})
 
-        mapped_nodes = get_mapped(g, 'HGNC', {'APP'})
+        g.add_node('chemokine protein family')
 
-        self.assertEqual(1, len(mapped_nodes))
-        self.assertIn('APP', mapped_nodes)
-        self.assertEqual({'APP Fragment'}, mapped_nodes['APP'])
+        g.add_edge('chemokine protein family', 'CCL2', **{RELATION: HAS_MEMBER})
+        g.add_edge('chemokine protein family', 'CCR2', **{RELATION: HAS_MEMBER})
+
+        g.add_edge('complex(p(HGNC:CCL2), p(HGNC:CCR2))', 'CCL2', **{RELATION: HAS_COMPONENT})
+        g.add_edge('complex(p(HGNC:CCL2), p(HGNC:CCR2))', 'CCR2', **{RELATION: HAS_COMPONENT})
+
+        mapped_nodes = get_mapped(g, 'HGNC', {'CCL2', 'CCR2'})
+
+        self.assertEqual(2, len(mapped_nodes))
+        self.assertIn('CCL2', mapped_nodes)
+        self.assertIn('CCR2', mapped_nodes)
+
+        self.assertEqual({'complex(p(HGNC:CCL2), p(HGNC:CCR2))', 'chemokine protein family'}, mapped_nodes['CCR2'])
+        self.assertEqual({'complex(p(HGNC:CCL2), p(HGNC:CCR2))', 'chemokine protein family'}, mapped_nodes['CCR2'])
