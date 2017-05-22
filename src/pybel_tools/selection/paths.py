8ba1b71d3db2985555625fae0f34b0d830c2b0f7
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-05-23 00:32:21 +0200

    Anything for the one liner
    Closes #73

--- src/pybel_tools/selection/paths.py
+++ src/pybel_tools/selection/paths.py
@@ -3,9 +3,11 @@
 
 import itertools as itt
 from itertools import product, tee
-from pybel.constants import *
+
 import networkx as nx
 from networkx import all_shortest_paths
+
+from pybel.constants import *
 
 __all__ = [
     'get_nodes_in_all_shortest_paths',
@@ -51,20 +53,13 @@ def rank_path(graph, path, edge_ranking=None):
 
     :param pybel.BELGraph graph: A BEL graph
     :param list[tuple] path: A list of nodes in the path (includes terminal nodes)
-    :param dict edge_ranking: relationship: score dictionary
+    :param dict edge_ranking: A dictionary of {relationship: score}
     :return: The score for the edge
     :rtype: int
     """
-
     edge_ranking = default_edge_ranking if edge_ranking is None else edge_ranking
 
-    path_score = 0
-
-    for u, v in pairwise(path):
-        path_score += max(
-            {edge_ranking[relation] for relation in {d[RELATION] for d in graph.edge[u][v].values()}})
-
-    return path_score
+    return sum(max(edge_ranking[d[RELATION]] for d in graph.edge[u][v].values()) for u, v in pairwise(path))
 
 
 def get_nodes_in_all_shortest_paths(graph, nodes, weight=None):
