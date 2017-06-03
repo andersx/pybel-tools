ea5d917ce3c10daa445fa62f4497304972c25cfe
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-06-03 22:32:05 +0200

    Add explicit hook for populate by graph

--- src/pybel_tools/integration/description/node_annotator.py
+++ src/pybel_tools/integration/description/node_annotator.py
@@ -28,8 +28,20 @@ class NodeAnnotator(object):
     def get_label(self, name):
         """Gets the label for the given name in. If not overridden, uses each node's name as its label."""
 
+    def populate_by_graph(self, graph):
+        """Optional hook for populating the annotator based on the nodes in a graph. Override this if
+        your node annotator downloads data ahead of time, such as grouping requests to an external API.
+
+        :param pybel.BELGraph graph: A BEL graph
+        """
+
     def annotate(self, graph):
-        """Annotates all nodes in this annotator's namespace"""
+        """Annotates all nodes in this annotator's namespace
+
+        :param pybel.BELGraph graph: A BEL graph
+        """
+        self.populate_by_graph(graph)
+
         for node in get_nodes_by_namespace(graph, self.namespace):
             name = graph.get_node_name(node)
 
