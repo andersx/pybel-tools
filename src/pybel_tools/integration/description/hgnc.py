fb41392edb4098b8e947f1b5314bb234ef3f2a02
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-06-03 21:46:45 +0200

    Update node annotator class to be lazier

--- src/pybel_tools/integration/description/hgnc.py
+++ src/pybel_tools/integration/description/hgnc.py
@@ -32,7 +32,6 @@ class HGNCAnnotator(NodeAnnotator):
         """
         super(HGNCAnnotator, self).__init__('HGNC')
 
-        # if data isn't cached
         df = pd.read_csv(HGNC_ENTREZ_URL, sep='\t')
 
         self.hgnc_entrez = {
@@ -42,27 +41,52 @@ class HGNCAnnotator(NodeAnnotator):
 
         self.entrez_hgnc = {v: k for k, v in self.hgnc_entrez.items()}
 
+        #: A dictionary of {str hgnc gene symbol: str description}
         self.descriptions = {}
+        #: A dictionary of {str hgnc gene symbol: str label}
         self.labels = {}
 
         if autopopulate:
             self.populate_unconstrained(group_size, sleep_time)
 
-    def populate(self, entrez_ids, group_size=200, sleep_time=1):
-        for entrez_ids in grouper(group_size, entrez_ids):
-            for entrez_id, v in get_entrez_gene_data(entrez_ids).items():
+    def map_entrez_ids(self, entrez_ids):
+        """Maps a list of Entrez Gene Identifiers to HGNC Gene Symbols"""
+        return [self.entrez_hgnc[entrez] for entrez in entrez_ids]
 
+    def map_hgnc(self, hgnc_symbols):
+        """Maps a list of HGNC Gene Symbols to Entrez Gene Identifiers"""
+        return [self.hgnc_entrez[hgnc] for hgnc in hgnc_symbols]
+
+    def get_unpopulated_entrez(self, entrez_ids):
+        """Gets the Entrez Gene Identifiers from this list that aren't already cached"""
+        hgnc_symbols = self.map_entrez_ids(entrez_ids)
+        missing_hgnc_symbols = set(self.descriptions) - set(hgnc_symbols)
+        return self.map_hgnc(missing_hgnc_symbols)
+
+    def populate(self, entrez_ids, group_size=200, sleep_time=1):
+        """Download the descriptions from Entrez Gene Service for a given list of Entrez Gene Identifiers
+
+        :param iter entrez_ids: An iterable of Entrez Gene Identifiers
+        :param int group_size: The number of entrez gene id's to send per query
+        :param int sleep_time: The number of seconds to sleep between queries
+        """
+        unpopulated_entrez = self.get_unpopulated_entrez(entrez_ids)
+
+        for entrez_ids_group in grouper(group_size, unpopulated_entrez):
+            for entrez_id, data in get_entrez_gene_data(entrez_ids_group).items():
                 try:
                     hgnc = self.entrez_hgnc[int(entrez_id)]
 
-                    self.labels[hgnc] = v['description']
-                    self.descriptions[hgnc] = v['summary']
-                except:
-                    log.warning('Missing for EGID: %s. Dat: %s', entrez_id, v)
+                    self.labels[hgnc] = data['description']
+                    self.descriptions[hgnc] = data['summary']
+                except Exception:
+                    log.warning('Missing for EGID: %s. Dat: %s', entrez_id, data)
 
             time.sleep(sleep_time)
 
     def populate_unconstrained(self, group_size=200, sleep_time=1):
+        """Downloads all descriptions for all Entrez Gene Identifiers"""
+
         #: This variable keeps track of when the data was downloaded
         self.download_time = time.asctime()
 
@@ -73,7 +97,8 @@ class HGNCAnnotator(NodeAnnotator):
         )
 
     def populate_constrained(self, hgnc_symbols, group_size=200, sleep_time=1):
-        entrez_ids = [self.hgnc_entrez[hgnc] for hgnc in hgnc_symbols]
+        """Downloads the gene information only for genes in the list of HGNC Gene Symbols"""
+        entrez_ids = self.map_hgnc(hgnc_symbols)
 
         self.populate(
             entrez_ids=entrez_ids,
@@ -82,6 +107,10 @@ class HGNCAnnotator(NodeAnnotator):
         )
 
     def populate_by_graph(self, graph):
+        """Downloads the gene information only for genes in the given graph
+
+        :param pybel.BELGraph graph: A BEL graph
+        """
         self.populate_constrained(hgnc_symbols=get_names(graph, self.namespace))
 
     def get_description(self, name):
--- src/pybel_tools/integration/description/node_annotator.py
+++ src/pybel_tools/integration/description/node_annotator.py
@@ -7,29 +7,13 @@ import six
 from ...filters import get_nodes_by_namespace
 
 __all__ = [
-    'add_descriptions',
-    'NodeAnnotator',
+    'NodeAnnotator'
 ]
-
-
-def add_descriptions(graph, namespace, node_annotator):
-    """Adds descriptions to all nodes using a given node annotator instance.
-
-    Assumes iteration over a namespace that has names.
-
-    :param pybel.BELGraph graph: A BEL graph
-    :param str namespace: The namespace to interrogate
-    :param NodeAnnotator node_annotator: A node annotator for the given namespace
-    """
-    for node in get_nodes_by_namespace(graph, namespace):
-        name = graph.get_node_name(node)
-        description = node_annotator.get_description(name)
-        graph.set_node_description(node, description)
 
 
 @six.add_metaclass(abc.ABCMeta)
 class NodeAnnotator(object):
-    """A node annotator"""
+    """The base class for node annotators."""
 
     def __init__(self, namespace):
         """
@@ -39,12 +23,13 @@ class NodeAnnotator(object):
 
     @abc.abstractmethod
     def get_description(self, name):
-        """Gets the description for the given name in"""
+        """Gets the description for the given name in this annotator's namespace."""
 
     def get_label(self, name):
-        """Gets the label for the given name in. Worst case scenario, canonicalizes the node as the label"""
+        """Gets the label for the given name in. If not overridden, uses each node's name as its label."""
 
     def annotate(self, graph):
+        """Annotates all nodes in this annotator's namespace"""
         for node in get_nodes_by_namespace(graph, self.namespace):
             name = graph.get_node_name(node)
 
